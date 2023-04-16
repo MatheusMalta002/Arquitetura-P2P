4fd4229065cdca62427be3d02f3114b96b56d171
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-04-16 18:14:57 -0300

    adicionando tempo de criptografia

---
+++ config.py
@@ -0,0 +1,12 @@
+seed = ("127.0.0.1",8891)
+
+# O nó semente (também conhecido como bootstrap node) é um nó especial na rede P2P que
+# é usado para ajudar outros nós a se conectar entre si. É responsável por manter uma 
+# lista de pares (também chamados de nós) que estão atualmente na rede e por responder a
+# consultas de novos nós que desejam ingressar na rede. Quando um novo nó entra na rede
+# ele envia uma mensagem para o nó semente, que responde com a lista atualizada de pares 
+# na rede. Isso permite que o novo nó se conecte com outros nós e comece a enviar e receber 
+# mensagens. Sem o nó semente, os nós teriam que descobrir uns aos outros por conta própria,
+# o que seria difícil em uma rede grande e dinâmica.
+
+
---
+++ udp.py
@@ -0,0 +1,86 @@
+import socket
+import threading
+import sys
+import json
+
+"""As mensagens foram serializadas usando json para
+   poder enviar mensagens mais complexas como dicionários
+   e arrays.
+"""
+
+# Função para enviar mensagem como string para um endereço
+def enviar_mensagem_base(udp_socket, toA, mensagem):
+    udp_socket.sendto(mensagem.encode(), (toA[0], toA[1]))
+
+
+# Função para receber mensagem e retornar mensagem e endereço
+def receber_mensagem_base(udp_socket):
+    data, addr = udp_socket.recvfrom(4096)
+    return data.decode(), addr
+
+
+# Função para enviar mensagem como JSON para um endereço
+def enviar_mensagem_JSON(udp_socket, toA, mensagem):
+    enviar_mensagem_base(udp_socket, toA, json.dumps(mensagem))
+
+
+# Função para enviar mensagem para vários peers
+def broadcast_mensagem_base(udp_socket, mensagem, peers):
+    for p in peers.values():
+        enviar_mensagem_base(udp_socket, p, mensagem)
+
+
+# Função para enviar mensagem como JSON para vários peers
+def broadcast_mensagem_JSON(udp_socket, mensagem, peers):
+    for p in peers.values():
+        enviar_mensagem_JSON(udp_socket, p, mensagem)
+
+
+def broadcast_mensagem_JSON_exclui_user(udp_socket, mensagem, user, peers, dicionario_msgs):
+    remetente = str(user)
+
+    if remetente in peers:
+        del peers[remetente]
+    else:
+        pass
+
+    for p in peers:
+
+        mensagem_criptada = dicionario_msgs[p]
+
+        mensagem['data'] = mensagem_criptada
+
+        enviar_mensagem_JSON(udp_socket, peers[p], mensagem)
+
+
+# Thread para receber mensagens
+def receber(udp_socket):
+    while 1:
+        data, addr = receber_mensagem_base(udp_socket)
+        print(data)
+
+
+# Thread para enviar mensagens
+def enviar(udp_socket):
+    while 1:
+        msg = input("Por favor, digite a mensagem e a porta:")
+        l = msg.split()
+        porta = int(l[-1])
+        s = ' '.join(l[:-1])
+        toA = ('127.0.0.1', porta)
+        enviar_mensagem_base(udp_socket, toA, s)
+
+
+def main():
+    porta = int(sys.argv[1])  # Obtém a porta a partir da linha de comando
+    fromA = ("127.0.0.1", porta)
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((fromA[0], fromA[1]))
+    t1 = threading.Thread(target=receber, args=(udp_socket,))
+    t2 = threading.Thread(target=enviar, args=(udp_socket,))
+    t1.start()
+    t2.start()
+
+
+if __name__ == '__main__':
+    main()
